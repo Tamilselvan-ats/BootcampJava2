1a11134ed5d67b173ca14d2245f4dae04c1e4261
Tamilselvan-ats <ts4490797@gmail.com>
2026-03-03 12:52:18 +0530

    commit successfull

---
+++ Age.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Age {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int birth_year=sc.nextInt();
+		System.out.println("Harry's age in 2024 is "+(2024-birth_year));
+	}
+}
